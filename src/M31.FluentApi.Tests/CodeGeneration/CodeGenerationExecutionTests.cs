6d7aeddc6ad32b0fb8e3e20308f4dc3092469473
Kevin Schaal <kevin.schaal@m31coding.com>
2024-05-14 08:35:50 +0200

    test: CanExecuteFluentReturnSingleStepPrivateMethodsClass and CanExecuteFluentReturnMultiStepPrivateMethodsClass

--- src/M31.FluentApi.Tests/CodeGeneration/CodeGenerationExecutionTests.cs
+++ src/M31.FluentApi.Tests/CodeGeneration/CodeGenerationExecutionTests.cs
@@ -119,5 +119,47 @@ public partial class CodeGenerationTests
             .CreateStudent.Method1<int, int>(in int0, ref string2);
         Assert.Equal(new string[] { "Called Method1<S, T>(in T, ref string)" }, student7.Logs);
     }
+
+    [Fact, Priority(1)]
+    public void CanExecuteFluentReturnSingleStepPrivateMethodsClass()
+    {
+        string string1 = "string1";
+
+        TestClasses.Abstract.FluentReturnSingleStepPrivateMethodsClass
+            .CreateStudent.ReturnVoidMethod();
+
+        int result1 = TestClasses.Abstract.FluentReturnSingleStepPrivateMethodsClass
+            .CreateStudent.ReturnIntMethod();
+        Assert.Equal(24, result1);
+
+        int result2 = TestClasses.Abstract.FluentReturnSingleStepPrivateMethodsClass
+            .CreateStudent.ReturnIntMethodWithRefParameter(ref string1);
+        Assert.Equal(28, result2);
+
+        List<int> result3 = TestClasses.Abstract.FluentReturnSingleStepPrivateMethodsClass
+            .CreateStudent.ReturnListMethod();
+        Assert.Equal(new List<int>() { 1, 2, 3 }, result3);
+    }
+
+    [Fact, Priority(1)]
+    public void CanExecuteFluentReturnMultiStepPrivateMethodsClass()
+    {
+        string string1 = "string1";
+
+        TestClasses.Abstract.FluentReturnMultiStepPrivateMethodsClass
+            .CreateStudent.WithName("Alice").ReturnVoidMethod();
+
+        int result1 = TestClasses.Abstract.FluentReturnMultiStepPrivateMethodsClass
+            .CreateStudent.WithName("Alice").ReturnIntMethod();
+        Assert.Equal(24, result1);
+
+        int result2 = TestClasses.Abstract.FluentReturnMultiStepPrivateMethodsClass
+            .CreateStudent.WithName("Alice").ReturnIntMethodWithRefParameter(ref string1);
+        Assert.Equal(28, result2);
+
+        List<int> result3 = TestClasses.Abstract.FluentReturnMultiStepPrivateMethodsClass
+            .CreateStudent.WithName("Alice").ReturnListMethod();
+        Assert.Equal(new List<int>() { 1, 2, 3 }, result3);
+    }
 }
 #endif
