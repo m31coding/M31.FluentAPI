e21a64f9911af714227dc0d598f91789e35aa2bb
Kevin Schaal <kevin.schaal@m31coding.com>
2024-05-15 18:59:02 +0200

    test: ContinueWithOfOverloadedMethodClass

---
+++ src/M31.FluentApi.Tests/CodeGeneration/TestClasses/Abstract/ContinueWithOfOverloadedMethodClass/Student.cs
@@ -0,0 +1,28 @@
+// Non-nullable member is uninitialized
+#pragma warning disable CS8618
+
+using M31.FluentApi.Attributes;
+
+namespace M31.FluentApi.Tests.CodeGeneration.TestClasses.Abstract.ContinueWithOfOverloadedMethodClass;
+
+[FluentApi]
+public class Student
+{
+    [FluentMethod(0)]
+    [FluentContinueWith(1)]
+    public void Method1()
+    {
+    }
+
+    [FluentMethod(0)]
+    [FluentContinueWith(2)]
+    public void Method1(int p1)
+    {
+    }
+
+    [FluentMember(1)]
+    public string Property1 { get; private set; }
+
+    [FluentMember(2)]
+    public string Property2 { get; private set; }
+}
