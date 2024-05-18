59965de6420542a888f4d1d9ae5ff071b859d28a
Kevin Schaal <kevin.schaal@m31coding.com>
2024-05-18 10:58:19 +0200

    test: EmptyClass

---
+++ src/M31.FluentApi.Tests/CodeGeneration/TestClasses/Abstract/EmptyClass/Student.cs
@@ -0,0 +1,11 @@
+// Non-nullable member is uninitialized
+#pragma warning disable CS8618
+
+using M31.FluentApi.Attributes;
+
+namespace M31.FluentApi.Tests.CodeGeneration.TestClasses.Abstract.EmptyClass;
+
+[FluentApi]
+public class Student
+{
+}
