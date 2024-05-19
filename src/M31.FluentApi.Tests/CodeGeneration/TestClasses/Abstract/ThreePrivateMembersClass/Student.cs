8068c546e4543ce881f87818e845ce78e8489be2
Kevin Schaal <kevin.schaal@m31coding.com>
2024-05-19 18:25:16 +0200

    test: ThreePrivateMembersClass

---
+++ src/M31.FluentApi.Tests/CodeGeneration/TestClasses/Abstract/ThreePrivateMembersClass/Student.cs
@@ -0,0 +1,20 @@
+// Non-nullable member is uninitialized
+#pragma warning disable CS8618
+
+using System;
+using M31.FluentApi.Attributes;
+
+namespace M31.FluentApi.Tests.CodeGeneration.TestClasses.Abstract.ThreePrivateMembersClass;
+
+[FluentApi]
+public class Student
+{
+    [FluentMember(0, "WithName")]
+    public string Name { get; private set; }
+
+    [FluentMember(1, "BornOn")]
+    public DateOnly DateOfBirth{ get; private set; }
+
+    [FluentMember(2, "InSemester")]
+    public int Semester { get; private set; }
+}
