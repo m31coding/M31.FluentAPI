52424e037eb9da86ce7f8eb3bf83f06e948b9439
Kevin Schaal <kevin.schaal@m31coding.com>
2024-05-26 13:12:08 +0200

    improve(ExampleProject): add person examples to Program.cs

--- src/ExampleProject/Program.cs
+++ src/ExampleProject/Program.cs
@@ -1,10 +1,25 @@
-﻿using ExampleProject;
+﻿using System.Text.Json;
+using ExampleProject;
 
-Student alice = CreateStudent.Named("Alice", "King").OfAge(22).WhoStartsUniversity()
+// Student
+//
+
+Student student1 = CreateStudent.Named("Alice", "King").OfAge(22).WhoStartsUniversity()
     .LivingIn("New York").WhoIsHappy().WhoseFriendsAre("Bob", "Carol", "Eve");
-
-Student bob = CreateStudent.Named("Bob", "Bishop").BornOn(new DateOnly(2002, 8, 3)).InSemester(2)
+Student student2 = CreateStudent.Named("Bob", "Bishop").BornOn(new DateOnly(2002, 8, 3)).InSemester(2)
     .LivingInBoston().WithUnknownMood().WhoseFriendIs("Alice");
 
-Console.WriteLine($"Alice: \"Hi {alice.Friends.First()}!\"");
-Console.WriteLine($"Bob: \"Hi {bob.Friends.First()}!\"");
+Console.WriteLine(JsonSerializer.Serialize(student1));
+Console.WriteLine(JsonSerializer.Serialize(student2));
+
+// Person
+//
+
+Person person1 = CreatePerson.WithFirstName("Alice").WithMiddleName("Sophia").WithLastName("King").WhoLivesAtAddress()
+    .WithHouseNumber("23").WithStreet("Market Street").InCity("San Francisco");
+Person person2 = CreatePerson.WithFirstName("Bob").WithLastName("Bishop").WhoseAddressIsUnknown();
+Person person3 = CreatePerson.WithFirstName("Eve").WithLastName("Johnson").WhoIsADigitalNomad().LivingInCity("Berlin");
+
+Console.WriteLine(JsonSerializer.Serialize(person1));
+Console.WriteLine(JsonSerializer.Serialize(person2));
+Console.WriteLine(JsonSerializer.Serialize(person3));
