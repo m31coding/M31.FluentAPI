01b941dd898f8b221e5755d6e3a6e5b289faf274
Kevin Schaal <kevin.schaal@m31coding.com>
2024-06-07 23:30:19 +0200

    feat(FluentSkippable): loop handling classes

---
+++ src/M31.FluentApi.Generator/CodeGeneration/CodeBoardActors/BuilderStepsGeneration/LoopHandling/DependencyLoop.cs
@@ -0,0 +1,32 @@
+namespace M31.FluentApi.Generator.CodeGeneration.CodeBoardActors.BuilderStepsGeneration.LoopHandling;
+
+/// <summary>
+/// Interfaces that form a circular dependency.
+/// </summary>
+internal class DependencyLoop
+{
+    private DependencyLoop(IReadOnlyCollection<BuilderInterface> interfaces, string commonInterfaceName)
+    {
+        Interfaces = interfaces;
+        CommonInterfaceName = commonInterfaceName;
+    }
+
+    internal IReadOnlyCollection<BuilderInterface> Interfaces { get; }
+    internal string CommonInterfaceName { get; }
+
+    internal static DependencyLoop Create(IReadOnlyCollection<BuilderInterface> interfaces)
+    {
+        string commonInterfaceName = GetCommonInterfaceName(interfaces.SelectMany(i => i.Methods).ToArray());
+        return new DependencyLoop(interfaces, commonInterfaceName);
+    }
+
+    private static string GetCommonInterfaceName(IReadOnlyCollection<InterfaceBuilderMethod> methods)
+    {
+        return $"I{string.Join(string.Empty, methods.Select(m => m.InterfaceName).Distinct().Select(RemoveCapitalI))}";
+
+        static string RemoveCapitalI(string interfaceName)
+        {
+            return interfaceName.Substring(1, interfaceName.Length - 1);
+        }
+    }
+}
