0c479cea5253ac864c3069e5417a76c939e08ee7
Kevin Schaal <kevin.schaal@m31coding.com>
2025-08-07 15:16:20 +0200

    test: DocumentedStudentClass

--- src/M31.FluentApi.Tests/CodeGeneration/TestDataProvider.cs
+++ src/M31.FluentApi.Tests/CodeGeneration/TestDataProvider.cs
@@ -107,6 +107,7 @@ internal class TestDataProvider : IEnumerable<object[]>
                 new object[] { "Abstract", "TwoMemberClass", "Student" },
                 new object[] { "Abstract", "TwoParameterCompoundClass", "Student" },
                 new object[] { "Abstract", "TwoParameterCompoundClassReversedParameters", "Student" },
+                new object[] { "DocumentedStudentClass", "DocumentedStudent" },
                 new object[] { "PersonClass", "Person" },
                 new object[] { "StudentClass", "Student" }
             }).Select(l => new string[] { "..", "..", "..", "CodeGeneration", "TestClasses" }
