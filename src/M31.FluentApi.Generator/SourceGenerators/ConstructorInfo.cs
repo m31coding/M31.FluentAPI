e93a406eace8126fdfbfb2e8af6c60e4acc24d89
Kevin Schaal <kevin.schaal@m31coding.com>
2025-12-28 13:50:00 +0100

    fix: equality tests

--- src/M31.FluentApi.Generator/SourceGenerators/ConstructorInfo.cs
+++ src/M31.FluentApi.Generator/SourceGenerators/ConstructorInfo.cs
@@ -1,10 +1,11 @@
 using M31.FluentApi.Generator.CodeGeneration.CodeBoardElements;
+using M31.FluentApi.Generator.Commons;
 
 namespace M31.FluentApi.Generator.SourceGenerators;
 
-internal record ConstructorInfo
+internal class ConstructorInfo
 {
-    public ConstructorInfo(IReadOnlyCollection<ParameterSymbolInfo> parameterInfos, bool constructorIsNonPublic)
+    internal ConstructorInfo(IReadOnlyCollection<ParameterSymbolInfo> parameterInfos, bool constructorIsNonPublic)
     {
         ParameterInfos = parameterInfos;
         ConstructorIsNonPublic = constructorIsNonPublic;
@@ -13,4 +14,25 @@ internal record ConstructorInfo
     internal IReadOnlyCollection<ParameterSymbolInfo> ParameterInfos { get; }
     internal bool ConstructorIsNonPublic { get; }
     internal int NumberOfParameters => ParameterInfos.Count;
+
+    protected bool Equals(ConstructorInfo other)
+    {
+        return ParameterInfos.SequenceEqual(other.ParameterInfos) &&
+               ConstructorIsNonPublic == other.ConstructorIsNonPublic;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((ConstructorInfo)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return new HashCode()
+            .AddSequence(ParameterInfos)
+            .Add(ConstructorIsNonPublic);
+    }
 }
