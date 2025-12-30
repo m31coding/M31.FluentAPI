cfa661e265d64d4f1494fee03c167a5a0fb3969e
Kevin Schaal <kevin.schaal@m31coding.com>
2025-12-30 11:00:32 +0100

    test: GenericOverloadedPrivateMethodClass

--- src/M31.FluentApi.Tests/CodeGeneration/TestClasses/Abstract/GenericOverloadedPrivateMethodClass/UsageTests.cs
+++ src/M31.FluentApi.Tests/CodeGeneration/TestClasses/Abstract/GenericOverloadedPrivateMethodClass/UsageTests.cs
@@ -13,28 +13,28 @@ public class UsageTests
     public void CanExecuteGenericOverloadedPrivateMethodClassTest1()
     {
         Student student = CreateStudent.Method1(0, "string1");
-        Assert.Equal(new string[] { "Called Method1(int, string)" }, student.Logs);
+        Assert.Equal(new[] { "Called Method1(int, string)" }, student.Logs);
     }
 
     [Fact, Priority(1)]
     public void CanExecuteGenericOverloadedPrivateMethodClassTest2()
     {
         Student student = CreateStudent.Method1<int>(0, "string1");
-        Assert.Equal(new string[] { "Called Method1<T>(int, string)" }, student.Logs);
+        Assert.Equal(new[] { "Called Method1<T>(int, string)" }, student.Logs);
     }
 
     [Fact, Priority(1)]
     public void CanExecuteGenericOverloadedPrivateMethodClassTest3()
     {
         Student student = CreateStudent.Method1<string>("string1", "string2");
-        Assert.Equal(new string[] { "Called Method1<T>(T, string)" }, student.Logs);
+        Assert.Equal(new[] { "Called Method1<T>(T, string)" }, student.Logs);
     }
 
     [Fact, Priority(1)]
     public void CanExecuteGenericOverloadedPrivateMethodClassTest4()
     {
         Student student = CreateStudent.Method1<int, int>(0, "string1");
-        Assert.Equal(new string[] { "Called Method1<S, T>(T, string)" }, student.Logs);
+        Assert.Equal(new[] { "Called Method1<S, T>(T, string)" }, student.Logs);
     }
 
     [Fact, Priority(1)]
@@ -42,7 +42,7 @@ public class UsageTests
     {
         string string1 = "string1";
         Student student = CreateStudent.Method1<int, int>(0, out string1);
-        Assert.Equal(new string[] { "Called Method1<S, T>(T, out string)" }, student.Logs);
+        Assert.Equal(new[] { "Called Method1<S, T>(T, out string)" }, student.Logs);
     }
 
     [Fact, Priority(1)]
@@ -50,7 +50,7 @@ public class UsageTests
     {
         int i = 0;
         Student student = CreateStudent.Method1<int, int>(in i, "string1");
-        Assert.Equal(new string[] { "Called Method1<S, T>(in T, string)" }, student.Logs);
+        Assert.Equal(new[] { "Called Method1<S, T>(in T, string)" }, student.Logs);
     }
 
     [Fact, Priority(1)]
@@ -59,7 +59,7 @@ public class UsageTests
         int i = 0;
         string string1 = "string";
         Student student = CreateStudent.Method1<int, int>(in i, ref string1);
-        Assert.Equal(new string[] { "Called Method1<S, T>(in T, ref string)" }, student.Logs);
+        Assert.Equal(new[] { "Called Method1<S, T>(in T, ref string)" }, student.Logs);
     }
 }
 
